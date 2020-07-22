a399aa2cbaceca329c05b40b2fe2e3c68bf52fd0
tuguldur <aa402115@gmail.com>
2020-07-22 19:02:37 +0800

    fix

--- index.js
+++ index.js
@@ -2,14 +2,10 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-process.env.DEV == "false" &&
-  app.use(express.static(path.join(__dirname, "/client/build")));
-
+app.use(express.static(path.join(__dirname, "/client/build")));
 app.use(express.json());
-
 app.use("/api", require("./src/routes"));
-process.env.DEV == "false" &&
-  app.get("*", (req, res) =>
-    res.sendFile(path.join(__dirname + "/client/build/index.html"))
-  );
-app.listen(process.env.PORT || 5000);
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname + "/client/build/index.html"))
+);
+app.listen(process.env.PORT || 5000, "0.0.0.0");
