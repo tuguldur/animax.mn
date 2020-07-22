ff9085844da1d87390e21c5fdba814e5a15e4281
tuguldur <aa402115@gmail.com>
2020-07-22 14:15:37 +0800

    Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: PORT=5000 DEV=false node index.js
--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-require("dotenv").config();
 
 process.env.DEV == "false" &&
   app.use(express.static(path.join(__dirname, "/client/build")));
